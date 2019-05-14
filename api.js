67b6c85309e6970930f1ae4622cf76c3e995539d
fqder897 <50565445+fqder897@users.noreply.github.com>
2019-05-14 08:59:17 +0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,14 @@
+$.getScript("https://open.mobile.qq.com/sdk/qqapi.js?_bid=152", function() {
+ mqq.ui.setTitleButtons({
+   left : {
+       title : "",
+       callback : function () {
+          //
+       }
+   },
+   right : {
+       hidden: true
+   }
+})
+  
+});
